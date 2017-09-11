b1def3fa675e2a4a711e6a21e544bd851ee83c53
rfocagal <lfogaca@fitec.org.br>
2017-09-11 17:36:59 -0300

    Created html elements, css and a few js functions

---
+++ priscila.js
@@ -0,0 +1,20 @@
+function deleteMe(e) {
+  e.parentNode.parentNode.removeChild(e.parentNode)
+}
+
+function addTask() {
+  var clone = document.getElementById("taskTemplate").cloneNode(true);
+  var element = document.getElementById("taskList");
+  clone.removeAttribute("id");
+  element.appendChild(clone);
+}
+
+document.getElementById("taskButton").onclick = addTask;
+
+function showDetail(){
+	
+}
+
+function hideDetail(){
+
+}
