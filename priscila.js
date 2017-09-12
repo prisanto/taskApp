d0513ad1f72a2a18f417e5413d4f4826966537eb
prisantos <prihsantos90@gmail.com>
2017-09-12 16:15:11 -0300

    Creating js to show task detail

--- priscila.js
+++ priscila.js
@@ -1,16 +1,58 @@
+//Cria a lista de informacao de tarefas e a lista de elementos dom da tarefa
+var taskList = [];
+var taskListDOM = [];
+
 function deleteMe(e) {
   e.parentNode.parentNode.removeChild(e.parentNode)
 }
 
+/*
 function addTask() {
   var clone = document.getElementById("taskTemplate").cloneNode(true);
   var element = document.getElementById("taskList");
   clone.removeAttribute("id");
   element.appendChild(clone);
+  document.getElementById("taskDetail").style.display = 'block';
+}
+*/
+
+function createTask(newTitle, newDescription){
+	return {title:newTitle, description:newDescription};
+}
+
+function createTaskDOM(){
+	var clone = document.getElementById("taskTemplate").cloneNode(true);
+  	clone.removeAttribute("id");
+    return clone;
+}
+
+function addTask() {
+    var task = createTask();
+    taskList.push(task);
+    var domElement = createTaskDOM();
+    taskListDOM.push(domElement);
+    var element = document.getElementById("taskList");
+    element.appendChild(domElement);
+    showTaskDetail(task);
+}
+
+
+
+function showTaskDetail(task){
+    var editingTask = taskList.indexOf(task);
+	document.getElementById("taskDetail").style.display = 'block';
+}
+
+function hideTaskDetail(){
+	document.getElementById("taskDetail").style.display = 'none';
+}
+
+function updateTask(taskId){
+    taskList[taskiId]
+}
+
+function updateTaskDom(task){
+
 }
 
 document.getElementById("taskButton").onclick = addTask;
-
-function showDetail(){
-
-}
