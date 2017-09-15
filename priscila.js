bdc34591f372c867b612d9b331f9ae5729cc4aa7
Priscila <prihsantos90@gmail.com>
2017-09-14 22:15:18 -0300

    Changes in js

--- priscila.js
+++ priscila.js
@@ -15,8 +15,23 @@ function createTaskDOM(){
         taskId = taskListLen;
     }
     clone.id = taskId;
-    clone.addEventListener('click', showTaskDetail(clone.id));
+    clone.addEventListener('click', showTaskDetail(taskId));
     return clone;
+}
+
+function showTaskDetail(task) {
+    document.getElementById("taskDetail").style.display = 'block';
+    //var taskEditing = taskList.indexOf(task);
+    //console.log("TASK do detail: " + taskEditing);
+    updateTaskListDom(task);
+}
+
+function updateTaskListDom(task){
+    var taskName = document.getElementById("taskName");
+    var taskDetail = document.getElementById("taskDetail");
+    console.log("task passada: " + task);
+    taskName.value = taskList[task-1].title;
+    taskDetail.value = taskList[task-1].description;
 }
 
 function updateTaskList(){
@@ -47,18 +62,7 @@ function updateTask(taskId){
     taskList[taskiId]
 }
 
-function updateTaskDom(task){
-    var taskName = document.getElementById("taskName");
-    var taskDetail = document.getElementById("taskDetail");
-    taskName.value = taskList[task].title;
-    taskDetail.value = taskList[task].description;
-}
 
-function showTaskDetail(task) {
-    document.getElementById("taskDetail").style.display = 'block';
-    var taskEditing = taskList.indexOf(task);
-    updateTaskDom(taskEditing);
-}
 
 /*
 document.getElementById("taskList").addEventListener("click", function(e){
@@ -69,7 +73,16 @@ document.getElementById("taskList").addEventListener("click", function(e){
 
 function updTaskName(taskId) {
     var taskName = document.getElementById("taskName");
-    taskName.value = taskList[taskId].title;
+    console.log("ID da task no upd: " + taskEditing);
+
+    if(taskList.length){
+    	var nomeTarefa = taskList[taskEditing-1].title;
+    	taskList[taskEditing-1].title = taskName.value;
+    	var domInput = document.getElementById(taskEditing-1).getElementsByTagName('input');
+    	console.log("DOMINPT: " + domInput);
+    	domInput.value = taskName.value;
+	    updateTaskList;
+    }
 }
 
 function updTaskDetail(taskId) {
@@ -80,10 +93,10 @@ function updTaskDetail(taskId) {
 
 document.getElementById("taskButton").onclick = addTask;
 document.getElementById("closeDetail").onclick = hideTaskDetail;
-document.getElementById("calendar").onclick = updTaskName;
-document.getElementById("taskName").onfocusout = updTaskName(taskEditing);
+//document.getElementById("calendar").onclick = updTaskName;
 document.getElementById("taskDetail").onfocusout = updTaskDetail;
 
+document.getElementById("taskName").onblur = updTaskName;
 
 function deleteMe(e) {
   e.parentNode.parentNode.removeChild(e.parentNode)
